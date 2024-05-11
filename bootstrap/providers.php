819ea8953eb29d4ca8f53a0136822c4f946cab92
njugunamwangi <njugunamwangi.n@gmail.com>
2024-05-11 17:21:16 +0300

    Remove company panel

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,6 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\AppPanelProvider::class,
-    App\Providers\Filament\CompanyPanelProvider::class,
     App\Providers\VoltServiceProvider::class,
 ];
