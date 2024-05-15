466fad061290563c3ba3b2cee7d78e355bd02b82
njugunamwangi <njugunamwangi.n@gmail.com>
2024-05-15 23:19:51 +0300

    Install LaravelDaily Invoices

---
+++ lang/vendor/invoices/cs/invoice.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Czech Invoice Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'invoice'                => 'Faktura',
+    'serial'                 => 'Faktura č.',
+    'date'                   => 'Datum',
+    'seller'                 => 'Prodejce',
+    'buyer'                  => 'Nakupující',
+    'address'                => 'Adresa',
+    'code'                   => 'Zákaznické číslo.',
+    'vat'                    => 'DPH',
+    'phone'                  => 'Telefon',
+    'description'            => 'Popis',
+    'units'                  => 'Jednotky',
+    'quantity'               => 'Množství',
+    'price'                  => 'Cena',
+    'discount'               => 'Sleva',
+    'tax'                    => 'Daň',
+    'sub_total'              => 'Mezisoučet',
+    'total_discount'         => 'Celková sleva',
+    'taxable_amount'         => 'Zdanitelná částka',
+    'total_taxes'            => 'Celková daň',
+    'tax_rate'               => 'Daňová sazba',
+    'total_amount'           => 'Celková částka',
+    'pay_until'              => 'Datum splatnosti',
+    'amount_in_words'        => 'Množství slovy',
+    'amount_in_words_format' => '%s %s a %s %s',
+    'notes'                  => 'Poznámky',
+    'shipping'               => 'Doprava',
+    'paid'                   => 'Zaplaceno',
+    'due'                    => 'Splatnost',
+    'page'                   => 'Strana',
+];
---
+++ lang/vendor/invoices/de/invoice.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | German Invoice Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'invoice'                => 'Rechnung',
+    'serial'                 => 'Rechnungsnr.',
+    'date'                   => 'Rechnungsdatum',
+    'seller'                 => 'Verkäufer',
+    'buyer'                  => 'Käufer',
+    'address'                => 'Adresse',
+    'code'                   => 'Kundennr.',
+    'vat'                    => 'Steuer-Nr.',
+    'phone'                  => 'Telefon',
+    'description'            => 'Beschreibung',
+    'units'                  => 'Einheit',
+    'quantity'               => 'Menge',
+    'price'                  => 'Preis',
+    'discount'               => 'Rabatt',
+    'tax'                    => 'Steuer',
+    'sub_total'              => 'Gesamt',
+    'total_discount'         => 'Rabatt gesamt',
+    'taxable_amount'         => 'Steuerpflichtiger Betrag',
+    'total_taxes'            => 'Steuern gesamt',
+    'tax_rate'               => 'Steuersatz',
+    'total_amount'           => 'Rechnungsbetrag',
+    'pay_until'              => 'Bitte zahlen Sie bis zum',
+    'amount_in_words'        => 'Betrag in Wörtern',
+    'amount_in_words_format' => '%s %s und %s %s',
+    'notes'                  => 'Notizen',
+    'shipping'               => 'Versand',
+    'paid'                   => 'Bezahlt',
+    'due'                    => 'Fällig',
+    'page'                   => 'Seite',
+];
