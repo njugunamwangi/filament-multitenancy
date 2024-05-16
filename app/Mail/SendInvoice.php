c5376ec7247a40493828155835bd18b43fa2bf5b
njugunamwangi <njugunamwangi.n@gmail.com>
2024-05-16 17:04:38 +0300

    send invoice to customer

---
+++ app/Mail/SendInvoice.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Invoice;
+use Filament\Facades\Filament;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Attachment;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use LasseRafn\Initials\Initials;
+
+class SendInvoice extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    private Invoice $invoice;
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(Invoice $invoice)
+    {
+        $this->invoice = $invoice;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Invoice ' . $this->invoice->serial,
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'emails.send-invoice',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        $company = Filament::getTenant();
+
+        $name = 'invoice_'.(new Initials)->name($company->name)->length(str_word_count($company->name))->generate().'_'.str_pad($this->invoice->serial_number, 5, '0', STR_PAD_LEFT).'.pdf';
+
+        return [
+            Attachment::fromPath(storage_path('app/public/invoices/'.$name))
+                ->withMime('application/pdf'),
+        ];
+    }
+}
--- config/filesystems.php
+++ config/filesystems.php
@@ -60,6 +60,14 @@ return [
             'throw' => false,
         ],
 
+        'invoices' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/invoices'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
