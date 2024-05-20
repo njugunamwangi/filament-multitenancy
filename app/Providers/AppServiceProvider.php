a5c38d68a279c349a59d64adaeb108b051ec229d
njugunamwangi <njugunamwangi.n@gmail.com>
2024-05-20 16:55:43 +0300

    Install Panel Switch

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Role;
+use App\Models\User;
+use BezhanSalleh\PanelSwitch\PanelSwitch;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        PanelSwitch::configureUsing(function (PanelSwitch $panelSwitch) {
+
+            $panelSwitch->simple()
+                ->visible(fn (): bool => auth()->user()->hasRole(Role::ADMIN));
+        });
     }
 }
