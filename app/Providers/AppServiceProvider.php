d220d4f9630c39c7af3d1610faca324fc4002ee3
njugunamwangi <njugunamwangi.n@gmail.com>
2024-06-10 04:19:42 +0300

    Remove invoice templates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,15 +31,5 @@ class AppServiceProvider extends ServiceProvider
             $panelSwitch->simple()
                 ->visible(fn (): bool => auth()->user()->hasRole(Role::ADMIN));
         });
-
-        FilamentAsset::register([
-            Css::make('custom-stylesheet', public_path('invoices/css/style.css'))->loadedOnRequest(),
-            Css::make('font-stylesheet', public_path('invoices/fonts/font-awesome/css/font-awesome.min.css'))->loadedOnRequest(),
-            Css::make('bootstrap-stylesheet', public_path('invoices/css/bootstrap.min.css'))->loadedOnRequest(),
-            Js::make('jquery', public_path('invoices/js/jquery.min.js'))->loadedOnRequest(),
-            Js::make('jspdf', public_path('invoices/js/jspdf.min.js'))->loadedOnRequest(),
-            Js::make('html2canvas', public_path('invoices/js/html2canvas.js'))->loadedOnRequest(),
-            Js::make('js-app', public_path('invoices/js/app.js'))->loadedOnRequest(),
-        ], package: 'app');
     }
 }
