442a3f48d3cec86db205bdc0650c7b803f9f4faa
njugunamwangi <njugunamwangi.n@gmail.com>
2024-06-20 04:56:27 +0300

    Register Staff panel provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,5 +4,6 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\AppPanelProvider::class,
+    App\Providers\Filament\StaffPanelProvider::class,
     App\Providers\VoltServiceProvider::class,
 ];
